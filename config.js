55cc8194ea1e9855c08c5f32aad4f923a3556c78
Prem Sahni <sahniprem568@gmail.com>
2026-09-25 19:56:56 +0530

    fix: correct registration form URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the complete application form URL between the quotes when it is ready.
 // Both registration buttons use this single value.
-window.DESGNEA_CLUB_APPLICATION_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSc7X2L0ld_aEJMR1O2tn5XfrZN7Q3BdtgChN--xa5o_-sqtiA/viewform";
+window.DESGNEA_CLUB_APPLICATION_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSc7X2L0Id_aEJMR1O2tn5XfrZN7Q3BdtgChN--xa5o_-sqtiA/viewform";
